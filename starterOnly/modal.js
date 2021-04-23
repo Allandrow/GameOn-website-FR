a2d28fce2409cf76d1a72d3f675765a780e73d13
Cyril Tiger <cyriltiger@gmail.com>
2021-04-23 18:09:42 +0200

    WIP #2 validation

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -26,3 +26,18 @@ function launchModal() {
 function closeModal() {
   modalbg.style.display = 'none';
 }
+
+// form validation on submit
+function validate(e) {
+  // Form inputs values
+  const firstName = document.getElementById('first').value;
+  const lastName = document.getElementById('last').value;
+  const email = document.getElementById('email').value;
+  const birthDate = document.getElementById('birthdate').value;
+  const tournamentNumber = document.getElementById('quantity').value;
+
+  firstName.length <= 1 ? console.log('FAIL') : console.log('SUCCESS');
+  lastName.length <= 1 ? console.log('FAIL') : console.log('SUCCESS');
+
+  e.preventDefault();
+}
