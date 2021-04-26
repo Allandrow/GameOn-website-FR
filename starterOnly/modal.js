8332c571e2c4c0a68439e7eaa711b40b124c4f96
Cyril Tiger <cyriltiger@gmail.com>
2021-04-26 16:34:09 +0200

    Form validation - fonctions de validation des différents inputs #2

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -34,28 +34,49 @@ function validate() {
   const lastName = document.getElementById('last').value;
   const email = document.getElementById('email').value;
   const tournamentNumber = document.getElementById('quantity').value;
+  const radioGroupLocation = document.querySelectorAll("input[name='location']");
+  const conditionCheckBox = document.getElementById('checkbox1');
 
   // TESTS
-  console.log('FIRST NAME');
-  console.log(validTextInput(firstName));
-  console.log('LAST NAME');
-  console.log(validTextInput(lastName));
-  console.log('EMAIL');
-  console.log(validEmail(email));
-  console.log('TOURNAMENT NUMBER');
-  console.log(validNumberInput(tournamentNumber));
+  console.log('TEST FIRST NAME = ' + validTextInput(firstName));
+  console.log('LAST NAME = ' + validTextInput(lastName));
+  console.log('EMAIL = ' + validTextInput(email));
+  console.log('TOURNAMENT NUMBER = ' + validNumberInput(tournamentNumber));
+  console.log('VILLE SELECTIONNEE = ' + validRadioSelected(radioGroupLocation));
+  console.log('CGU VALIDEES = ' + checkboxChecked(conditionCheckBox));
 
   event.preventDefault();
 }
 
+// Check if input value is less than 2 characters
 function validTextInput(input) {
   return input.length <= 1 ? false : true;
 }
 
+// Check if email has valid syntax (any non whitepsace characters - @ - any non whitespace characters - . - any non whitespace characters)
 function validEmail(email) {
   return /\S+@+\S+\.\S+/.test(email);
 }
 
+// Check if input is empty
 function validNumberInput(input) {
   return input.length !== 0 ? true : false;
+}
+
+// Check if radio group has a selection
+function validRadioSelected(group) {
+  let selected = false;
+
+  group.forEach((radio) => {
+    if (radio.checked) {
+      selected = true;
+    }
+  });
+
+  return selected;
+}
+
+// Checkbox status
+function checkboxChecked(input) {
+  return input.checked ? true : false;
 }
